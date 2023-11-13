77665225959260f7072f094e7fac172f058f7572
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-11-14 00:05:42 +0600

    modal add

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,17 @@
 const startBtn = document.querySelector("#start-btn");
 
+// modal
+const modal = document.querySelector("#modal");
+
 // start button event listener
 startBtn.addEventListener("click", () => {
   const lowerValue = document.querySelector("#lower-number").value;
   const highValue = document.querySelector("#higher-number").value;
   if (!lowerValue) return errorPopupShow("Please enter a lower value");
   if (!highValue) return errorPopupShow("Please enter a higher value");
+
+  // modal show
+  modal.classList.remove("hidden");
 });
 
 // error popup function
@@ -20,3 +26,12 @@ function errorPopupShow(message) {
     errorPopup.classList.add("hidden");
   }, 3000);
 }
+
+// random number between two numbers
+function randomNumBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// innerHTML add in modal
+
+function modalInnerHTML() {}
